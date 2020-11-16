d321b67a058013762ea262fe82991ff9473b4a40
moazzamsyntax <moazzamsyntax@gmail.com>
2020-11-16 16:02:04 -0500

    1st

---
+++ src/syntaxgit/Car.java
@@ -0,0 +1,10 @@
+package syntaxgit;
+
+public class Car {
+
+		String color;
+		String model;
+		int doors;
+		
+	}
+ 
---
+++ src/syntaxgit/Main.java
@@ -0,0 +1,19 @@
+package syntaxgit;
+
+public class Main {
+	public static void main(String[] args) {
+		 
+		
+		Car Audi=new Car();
+		Audi.color="Black";
+		 
+		
+		
+		  
+		 
+		
+	    System.out.println(Audi.color);
+	   
+    }
+
+}
